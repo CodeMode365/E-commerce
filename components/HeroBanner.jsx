975be08a324ad89d8bea6be7a8a28dd91c288de0
CodeMode365 <codemode365@gmail.com>
2022-09-23 11:04:44 -0700

    banner creation

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,17 +1,28 @@
 import Link from "next/link";
 import React from "react";
+import { urlFor } from "../lib/client";
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
+  const { smallText, midText, largeText1,product,buttonText,desc } = heroBanner;
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src="" alt="headphone" className="hero-banner-image"/>
+        <p className="beats-solo">{smallText}</p>
+        <h3>{midText}</h3>
+        <h1>{largeText1}</h1>
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphone"
+          className="hero-banner-image"
+        />
         <div>
-          <Link href="/product/ID">
-            <button type="button">BUTTON TEXT</button>
+          <Link href={`/product/ID${product}`}>
+            <button type="button">{buttonText}</button>
           </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{desc}</p>
+          </div>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,31 @@
 import React from "react";
-import { Proudct, FooterBanner, HeroBanner } from "../components";
+import { Proudct, FooterBanner, HeroBanner, Footer } from "../components";
+import { client } from "../lib/client";
 
-const Home = () => {
+const Home = ({ products, banners }) => {
+  console.log(banners)
   return (
-    <>
-      <HeroBanner />
+    <div>
+      <HeroBanner heroBanner={banners} />
       <div className="products-heading">
         <h2>Beset Selling Product</h2>
         <p>Speakers of many vaiations</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
-    </>
+      <div>{products?.map((product) => product)}</div>
+      <Footer />
+    </div>
   );
 };
 
+export const getServerSideProps = async () => {
+  const query = '*[_type=="product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type=="product"]';
+  const banners = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, banners },
+  };
+};
 export default Home;
